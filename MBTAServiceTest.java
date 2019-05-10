f8d526dfde3f886ecc95e1f66cb44c0d70712817
Pammi Yeung <mushroomg@users.noreply.github.com>
2019-05-10 15:23:16 -0400

    modified MBTAServiceTest and FormatTextTest

--- MBTAServiceTest.java
+++ MBTAServiceTest.java
@@ -1,8 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.HashMap;
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 
 /** @author Pammi Yeung
@@ -16,22 +14,21 @@ class MBTAServiceTest {
 	@Test
 	void test() {
 		
-		HashMap<String, MBTAReply> sentTexts = new HashMap();
+		HashMap<String, MBTAReply> sentTexts = new HashMap<>();
 		
 		// implement test versions of GetData and TextSender interfaces
 		  GetData getData = new GetData() {
 
 			@Override
 			public ArrayList<MBTAReply> crawlMBTA(Request r) {
-				ArrayList<MBTAReply> replies = new ArrayList();
-				replies.add(new MBTAReply("Red", "1", "South Station", "Alewife", "8am"));
+				ArrayList<MBTAReply> replies = new ArrayList<>();
+				replies.add(new MBTAReply("Red", "place-alfcl", "0", null, "13:05"));
 				return replies;
 			}
 			  
 		  };
 		  
 		  TextSender textSender = new TextSender() {
-
 			@Override
 			public void sendText(String phoneNumber, ArrayList<MBTAReply> reply) {
 				// add phone number and reply to check what was sent
@@ -41,21 +38,33 @@ class MBTAServiceTest {
 		  };
 		  
 		MBTAService service = new MBTAService(getData, textSender);
-		Request req = new Request("Red", "South Station", "Alewife", "8am");
+		Request req = new Request("Red", "place-davis", "0", "08:05");
+		
+		// call the addEntry method
 		service.addEntry("6171234567",req);
 		HashMap<String, Request> modifiedHash = service.getHash();
 		
 		// create the expected Hash
 		HashMap<String,Request> expectedHash = new HashMap<>();
 		expectedHash.put("6171234567", req);
-		// compare them
+		
+		// First test for service: compare the expectedHash to modifiedHash
 		assertEquals(expectedHash, modifiedHash);
 		
+		// call the send method
 		service.send();
 		
 		// create the expected Hash
 		HashMap<String,MBTAReply> expectedSentHash = new HashMap<>();
-		expectedSentHash.put("6171234567",new MBTAReply("Red", "1", "South Station", "Alewife", "8am"));
+		
+		// line below is commented out tom keep exepectedSentHash null
+		// because the send() method checks if the current time is 08:05
+		// unless this MBTAServiceTest program is being run at exactly 08:05,
+		// the timeToText boolean will be false and the crawlMBTA and sendText methods will not be called
+		
+		//expectedSentHash.put("6171234567",new MBTAReply("Red", "place-alfcl", "0", null, "13:05"));
+		
+		// expect the crawlMBTA and sendText methods to not be called 
 		assertEquals(expectedSentHash, sentTexts);
 		
 		
